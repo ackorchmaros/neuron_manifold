c7ba69db89ca5dcd5e4e33fa32ea9e017d8870be
Annachiara Korchmaros <45043205+ackorchmaros@users.noreply.github.com>
2021-04-30 15:19:38 +0300

    changes on load main_example
    some comments added

--- fig234/load_main_example.m
+++ fig234/load_main_example.m
@@ -7,12 +7,13 @@ dataroot = sprintf('%s/data', mainpath);
 %% LOAD DATA
 % save mouse response to natimag2800 stimulus
 
-% our default mouse
+% our default mouse parameters
 mouse_tag=4;
 mouse='M170714_MP032_2017-08-07'; 
 
 K=1; % type of data:natimag2800
 
+% load files 
 load(fullfile(dataroot,'dbstims.mat')); %stimset
 load(fullfile(dataroot,sprintf('%s_proc.mat',stimset{K})));% respAll
 signal = double(respAll{mouse_tag});
