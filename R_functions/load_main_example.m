0ec4ffd3e98535aa36119d81ccf544adce2b605b
Annachiara Korchmaros <45043205+ackorchmaros@users.noreply.github.com>
2021-05-04 15:31:46 +0300

    add file

---
+++ R_functions/load_main_example.m
@@ -0,0 +1,23 @@
+clear all;
+
+% !!!!!! update mainpath 
+mainpath = 'Desktop/neuron_manifold';
+% where data is stored 
+dataroot = sprintf('%s/data', mainpath); 
+%% LOAD DATA
+% save mouse response to natimag2800 stimulus
+
+% our default mouse parameters
+mouse_tag=4;
+mouse='M170714_MP032_2017-08-07'; 
+
+K=1; % type of data:natimag2800
+
+% load files 
+load(fullfile(dataroot,'dbstims.mat')); %stimset
+load(fullfile(dataroot,sprintf('%s_proc.mat',stimset{K})));% respAll
+signal = double(respAll{mouse_tag});
+
+save(fullfile(dataroot,sprintf('clean_signal_%s.mat',mouse)), 'signal');
+
+disp('Done');
