b47b651832dd5aa8fe602d23d99bd2d2a551927f
Annachiara Korchmaros <45043205+ackorchmaros@users.noreply.github.com>
2021-05-04 14:24:04 +0300

    add files

---
+++ fig10/add_noise2segre.R
@@ -0,0 +1,82 @@
+#--------------------------------------Add random noise to Segre sample to make 2 repeats
+## author: Annachiara Korchmaros
+## comments: 
+#(1) load data matrices with 3000,300, or 30 samples 
+#(2) add noise from multivariate normal with different seed for each image 
+#(3) centralize repeats
+#(4) check correlation between repeats is 0.21 (tune mean and stsd checking this!)
+#(5) print correlation
+#(6) save repeats
+##############
+## libraries 
+##############
+library(base)
+library(R.matlab) 
+library(Rfast)
+##############
+## arguments 
+##############
+# input arguments
+args = commandArgs(trailingOnly=TRUE)
+ambient.dimension<-as.numeric(args[1]) 
+sample.size<-as.numeric(args[2])
+# output path
+out.dir<-args[3] 
+dir.create(out.dir, showWarnings = FALSE) 
+mean<-as.numeric(args[4]) 
+std<-as.numeric(args[5])
+# other parameters
+if (ambient.dimension== 70 | ambient.dimension== 40){
+  vector.length<-sqrt(100)
+}else{
+  vector.length<-sqrt(ambient.dimension)
+}
+segre.dimension<-(vector.length-1)*2+1
+##############
+## functions
+##############
+# center by column
+## source: https://www.gastonsanchez.com/visually-enforced/how-to/2014/01/15/Center-data-in-R/
+center_apply <- function(x) {
+  apply(x, 2, function(y) y - mean(y))
+}
+##############
+## steps
+##############
+#(1) 
+f.case.data<-paste0(out.dir,"/",ambient.dimension,"ambient_",sample.size,'sample_',segre.dimension,'segre.mat')
+case.data <- readMat(f.case.data) 
+case.data<-case.data$x
+#(2) 
+sample.mean<-rep(0,ambient.dimension)
+sample.covariance.matrix<-diag(std,ambient.dimension)
+sample.segre.repeat1<-matrix(0, nrow = sample.size, ncol = ambient.dimension)
+sample.segre.repeat2<-matrix(0, nrow = sample.size, ncol = ambient.dimension)
+
+print("Sampling..")
+for (i in 1:(sample.size*2)) {
+  set.seed(i)
+  noise.repeat1<-rmvnorm(n=ambient.dimension,mean, std)
+  noise.repeat2<-rmvnorm(n=ambient.dimension, mean, std)
+  if (i<=sample.size){
+    sample.segre.repeat1[i,]<-case.data[i,]+noise.repeat1
+  }else{
+    sample.segre.repeat2[(i-sample.size),]<-case.data[(i-sample.size),]+noise.repeat2
+  }
+}
+
+#(3)
+sample.segre<-rbind(sample.segre.repeat1,sample.segre.repeat2)
+sample.segre.cent<-center_apply(sample.segre)
+sample.segre.repeat1<-sample.segre.cent[1:sample.size,]
+sample.segre.repeat2<-sample.segre.cent[(sample.size+1):(2*sample.size),]
+
+#(4) 
+correlation<-cor(as.vector(sample.segre.repeat1),as.vector(sample.segre.repeat2))
+print(paste0('The correlation between repeats is ',correlation))
+
+#(5) save
+f.case.data<-paste0(out.dir,"/",ambient.dimension,"ambient_",sample.size,'sample_',segre.dimension,'segre_repeat1.mat')
+writeMat(con=f.case.data,repeat1=sample.segre.repeat1)
+f.case.data<-paste0(out.dir,"/",ambient.dimension,"ambient_",sample.size,'sample_',segre.dimension,'segre_repeat2.mat')
+writeMat(con=f.case.data,repeat2=sample.segre.repeat2)
---
+++ fig10/sample_segre_cases_proj.R
@@ -0,0 +1,87 @@
+#--------------------------------------Sampling 1 repeat from Segre variety away from the origin (singular point)
+## author: Annachiara Korchmaros
+## comments: 
+#(1) choose 2 pairs of vectors (x,y) from two standard multivariate normal distributions
+#(2) get their segre product, ie (x1y1,x1y2,....) 
+#(3) check first coordinate isn't zero (check vectors first coordinate) and resample eventually (optional)
+#(4) pass to nonhomogeous coordinates and check points are projective different and resample eventually (optional)
+#(6) save 3000 sample size
+#(7) subsample 2 repeats of 300 samples each from (6) randomly
+#(8) subsample 2 repeats of 30 samples each from (6) randomly
+##############
+## libraries 
+##############
+library(base)
+library(R.matlab) 
+library(mvtnorm) 
+library(mgcv)
+##############
+## arguments 
+##############
+# input arguments
+args = commandArgs(trailingOnly=TRUE)
+ambient.dimension<-as.numeric(args[1])
+sample.size<-as.numeric(args[2]) 
+# output path
+out.dir<-args[3]
+dir.create(out.dir, showWarnings = FALSE) 
+Mean<-as.numeric(args[4]) 
+# other parameters
+vector.length<-sqrt(ambient.dimension)
+segre.dimension<-(vector.length-1)*2+1
+##############
+## function
+##############
+# Segre product
+segre.product<-function(x,y){
+  S<-NULL
+  for (i in 1:length(x)){
+      S<-append(S,x[i]*y)
+  }
+  return(S)
+}
+################
+## steps
+################
+sample.segre<-matrix(0, nrow = sample.size, ncol = ambient.dimension)
+sample.mean<-rep(Mean,vector.length)
+sample.covariance.matrix<-diag(vector.length)
+
+print("Sampling..")
+for (i in 1:sample.size){
+  #(1)
+  set.seed(i) 
+  x<-rmvnorm(n=1, sample.mean, sample.covariance.matrix)
+  set.seed(i+sample.size) 
+  y<-rmvnorm(n=1, sample.mean, sample.covariance.matrix)
+  x.S.y<-segre.product(x,y) #(2)
+  sample.segre[i,]<-x.S.y
+}
+#(3) 
+#length(which(sample.segre[,1]==0)) # none
+#(4) 
+#nonH.sample.segre<-sample.segre/sample.segre[,1]
+#no.duplicats.nonH.sample.segre<-uniquecombs(nonH.sample.segre) # all points are projectively different
+
+#(5)
+f.sample.segre<-paste0(out.dir,"/",ambient.dimension,'ambient_',sample.size,"sample_",segre.dimension,"segre.mat")
+writeMat(con=f.sample.segre,x=sample.segre)
+
+#(6) 
+subsample.size<-300
+set.seed(170714) # mouse tag
+index.subsample<-sample.int(sample.size, subsample.size) #integer version of sample function
+subsample.segre<-sample.segre[index.subsample,]
+
+f.subsample.segre<-paste0(out.dir,"/",ambient.dimension,'ambient_',subsample.size,"sample_",segre.dimension,"segre.mat")
+writeMat(con=f.subsample.segre,x=subsample.segre)
+
+#(7)
+subsample.size<-30
+set.seed(170715) 
+index.subsample<-sample.int(sample.size, subsample.size)
+subsample.segre<-sample.segre[index.subsample,]
+
+f.subsample.segre<-paste0(out.dir,"/",ambient.dimension,'ambient_',subsample.size,"sample_",segre.dimension,"segre.mat")
+writeMat(con=f.subsample.segre,x=subsample.segre)
+
