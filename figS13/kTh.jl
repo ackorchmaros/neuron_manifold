4acce289be185bd0aafa1c3f049aba9e766be59f
Annachiara Korchmaros <45043205+ackorchmaros@users.noreply.github.com>
2022-02-14 18:23:34 +0100

    Add files via upload

---
+++ figS13/kTh.jl
@@ -0,0 +1,58 @@
+######################################################### NPCA dimension estimate with tight threshold wrt to the number of nearest neighbours WRAPPER
+# author: annachiara korchmaros
+
+# load packages
+#using LearningAlgebraicVarieties
+using Clustering
+using Statistics
+using Colors
+using LinearAlgebra
+using MAT
+using ArgParse
+using Distributions
+using Random
+using Plots
+
+
+s = ArgParseSettings()
+@add_arg_table s begin
+    
+    "finput"
+        help = "path to data matrix as features X samples"
+        required = true
+    "fout"
+    help = "path to the output figure"
+    required = true
+
+    "fNN"
+        help = "path to data matrix with numbers of NN"
+        required = true
+end
+parsed_args = parse_args(ARGS, s)
+
+# load data
+input  =  parsed_args["finput"] 
+file = matopen(input)
+data=read(file)
+close(file)
+matrix_data=data["x"]
+
+NN  =  parsed_args["fNN"] 
+file = matopen(NN)
+data=read(file)
+close(file)
+matrix_NN=data["x"]
+
+
+# load function
+input_folder = dirname(input)
+cd(input_folder)
+include("k_cluster_th_savefig.jl")
+
+# compute dimension diagram
+output  =  parsed_args["fout"]
+println("Computing dimension estimate ..") 
+DimensionDiagramsNPCAnn(matrix_data, matrix_NN,2,output)
+
+println("Done!")
+
