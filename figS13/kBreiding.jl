c5f0b4210f2b513bad0938dda3d37aee3a90e087
Annachiara Korchmaros <45043205+ackorchmaros@users.noreply.github.com>
2022-02-14 16:37:43 +0100

    add file

---
+++ figS13/kBreiding.jl
@@ -0,0 +1,60 @@
+############################################################################################################# NPCA with Breiding threshold wrt k ID estimate 
+# author: annachiara korchmaros
+
+# load packages
+using Clustering
+using Statistics
+using Colors
+using LinearAlgebra
+using MAT
+using ArgParse
+using Distributions
+using Random
+using Plots
+
+s = ArgParseSettings()
+@add_arg_table s begin
+    
+    "finput"
+        help = "path to data matrix as features X samples"
+        required = true
+    "fout"
+    help = "path to the output figure"
+    required = true
+
+    "fNN"
+        help = "path to data matrix with numbers of NN"
+        required = true
+end
+parsed_args = parse_args(ARGS, s)
+
+# load data
+input  =  parsed_args["finput"] 
+file = matopen(input)
+data=read(file)
+close(file)
+matrix_data=data["x"]
+
+NN  =  parsed_args["fNN"] 
+file = matopen(NN)
+data=read(file)
+close(file)
+matrix_NN=data["x"]
+
+
+# load function
+input_folder = dirname(input)
+cd(input_folder)
+include("k_clusters_breiding_th_savefig.jl")
+
+# compute dimension diagram
+println("Computing dimension estimate ..") 
+output  =  parsed_args["fout"]
+DimensionDiagramsNPCAnn(matrix_data, matrix_NN,50,output)
+
+println("Done!")
+
+
+
+
+
